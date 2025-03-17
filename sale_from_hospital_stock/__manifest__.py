af6cb84de0c80ccb68e3182372de733fdb86d2e8
Alexis de Lattre <alexis.delattre@akretion.com>
2025-03-17 23:07:19 +0000

    [IMP] sale_from_hospital_stock: add filter on stock.quant

--- sale_from_hospital_stock/__manifest__.py
+++ sale_from_hospital_stock/__manifest__.py
@@ -17,6 +17,7 @@
         'views/sale_order.xml',
         'views/res_partner.xml',
         'views/stock_route.xml',
+        'views/stock_quant.xml',
         'views/stock_location.xml',
         'views/stock_picking.xml',
         'wizards/res_config_settings_view.xml',
